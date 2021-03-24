f28859d73ac95aedf6cdd06083df3e604933e66e
Jonáš Herzig <herzigj.03@in.spst.cz>
2021-03-24 17:46:42 +0100

    synchronizace

--- MetodyGit2/Form1.cs
+++ MetodyGit2/Form1.cs
@@ -16,14 +16,13 @@ namespace MetodyGit2
         {
             InitializeComponent();
         }
-        DateTime Za100Dni() //metoda funguje
+        DateTime Za100Dni(out DateTime datum) //metoda funguje
         {
-            DateTime dt = DateTime.Now;
-            DateTime datum100 = dt.AddDays(100);
-            return datum100;
+            datum = DateTime.Now.AddDays(100);
+            return datum;
         }
 
-        double Metoda2(double a) //umocní číslo z textbocu na druhou
+        double Metoda2(double a) //umocní číslo z textbocu na druhou, funguje
         {
             return a * a;
         }
@@ -31,7 +30,7 @@ namespace MetodyGit2
         private void button1_Click(object sender, EventArgs e)
         {
             DateTime dt = DateTime.Now;
-            DateTime datum100 = dt.AddDays(100);
+            DateTime datum100 = Za100Dni(out dt);
             MessageBox.Show("Za 100 dnů od aktuálního data bude " + datum100.ToString());
             double a = double.Parse(textBox1.Text);
             MessageBox.Show("Druhá mocnina čísla z textboxu je: " + Metoda2(a));
